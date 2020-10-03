8e360e55e0f03cdc1f289a8b96d252f4e2a9b61e
Nkuppili001 <navneel.kuppili@dteenergy.com>
2020-10-03 18:46:04 -0400

    Basic redux and router setup

---
+++ src/redux/configureStore.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, createStore, compose } from 'redux';
+import rootReducer from './reducers';
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+
+export default function configureStore(initialState) {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for Redux Dev tools
+    return createStore(
+        rootReducer,
+        initialState,
+        composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+    )
+}
---
+++ src/redux/reducers/courseReducer.js
@@ -0,0 +1,10 @@
+const { func } = require("prop-types");
+
+export default function courseReducer(state = [], action) {
+    switch(action.type) {
+        case "CREATE_COURSE":
+            return [...state, {...action.course}];
+        default:
+            return state;
+    }
+}
