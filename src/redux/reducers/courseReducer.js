f002782893cf420b374aacf66a2b553cc5742840
Nkuppili001 <navneel.kuppili@dteenergy.com>
2020-10-03 19:49:21 -0400

    created constants

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -1,8 +1,8 @@
-const { func } = require("prop-types");
+import * as types from '../actions/actionTypes';
 
 export default function courseReducer(state = [], action) {
     switch(action.type) {
-        case "CREATE_COURSE":
+        case types.CREATE_COURSE :
             debugger
             return [...state, {...action.course}];
         default:
