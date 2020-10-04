99580dfdc53c67d13774e9a6e3f8c3bb17b79590
Nkuppili001 <navneel.kuppili@dteenergy.com>
2020-10-04 17:31:54 -0400

    added thunk and updated dispatch handling

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,13 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import rootReducer from './reducers';
-import reduxImmutableStateInvariant from 'redux-immutable-state-invariant'
+import reduxImmutableStateInvariant from 'redux-immutable-state-invariant';
+import thunk from 'redux-thunk';
 
 export default function configureStore(initialState) {
     const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; //add support for Redux Dev tools
     return createStore(
         rootReducer,
         initialState,
-        composeEnhancers(applyMiddleware(reduxImmutableStateInvariant()))
+        composeEnhancers(applyMiddleware(thunk, reduxImmutableStateInvariant()))
     )
 }
