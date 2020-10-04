355db55f378990ea6de556e09277b7b39ed17835
Nkuppili001 <navneel.kuppili@dteenergy.com>
2020-10-04 17:33:10 -0400

    added authors actions/reducers and updated courses page

--- src/redux/reducers/courseReducer.js
+++ src/redux/reducers/courseReducer.js
@@ -3,8 +3,9 @@ import * as types from '../actions/actionTypes';
 export default function courseReducer(state = [], action) {
     switch(action.type) {
         case types.CREATE_COURSE :
-            debugger
             return [...state, {...action.course}];
+        case types.LOAD_COURSES_SUCCESS :
+            return action.courses;
         default:
             return state;
     }
